f9e7fd952248e4a9b8956db1b07bf95d61d67547
Nataniel Santos <nathan.santos159@hotmail.com>
2023-10-07 19:26:00 -0300

    refac: DayState icon component

---
+++ src/components/DayStateIcon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import doneIcon from '../assets/done.png';
+import grayIcon from '../assets/gray.png';
+import unfinishedIcon from '../assets/unfinished.png';
+
+function DayStateIcon({ isDone, onClick }) {
+  let image;
+  const defaultProps = {
+    height: 20,
+    width: 20,
+    onClick,
+  }
+
+  if (isDone === true) {
+    image = <img src={doneIcon} alt="Done Icon" {...defaultProps}/>
+  } else if (isDone === false){
+    image = <img src={unfinishedIcon} alt="Unfinished Icon" {...defaultProps}/>
+  } else if (isDone === undefined) {
+    image = <img src={grayIcon} alt="Gray Icon" {...defaultProps}/>
+  }
+
+  return image
+}
+
+export default DayStateIcon;
