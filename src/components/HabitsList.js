164b368128ffc22cadd9b162133aeb90ce163a23
Nataniel Santos <nathan.santos159@hotmail.com>
2023-10-07 19:25:16 -0300

    feat: Creates HabitsContext

--- src/components/HabitsList.js
+++ src/components/HabitsList.js
@@ -1,32 +1,14 @@
 import {
-  Grid,
-  GridItem,
   Text,
   Box,
-  IconButton,
-  Flex,
   Spinner,
 } from '@chakra-ui/react';
-import DayState from '../components/DayState';
-import deleteIcon from '../assets/delete.png';
-import useCloudStorage from '../hooks/useCloudStorage';
-import { useState, useEffect } from 'react';
-import { getWeekDates } from '../utils/getWeekDates';
+import { useContext } from 'react';
+import HabitCard from './HabitCard';
+import { HabitsContext } from '../context/HabitsContext';
 
-function HabitsList(props) {
-  const [habits, setHabits] = useState(null);
-  const cloudStorage = useCloudStorage();
-  const [weekDates] = useState(getWeekDates());
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    cloudStorage.getArray('habits')
-      .then((response) => {
-        console.log(response);
-        setHabits(response);
-        setIsLoading(false);
-      })
-  }, []);
+function HabitsList() {
+  const { habits, isLoading } = useContext(HabitsContext);
 
   return (
     <>
@@ -35,39 +17,9 @@ function HabitsList(props) {
           <Text textColor="var(--tg-theme-hint-color)" fontSize="xl" textAlign="center" m="10">No habits yet.</Text>
         ))
       }
-      {isLoading && <Spinner size='sm' marginY="10" />}
-      {habits && Array.isArray(habits) && habits.map(({ name, history, selectedWeekDays }) => (
-        <Box textColor="var(--tg-theme-text-color)">
-          <Flex justifyContent="space-between">
-            <Text
-              fontSize="xl"
-              css={{ fontWeight: '300' }}
-            >{name}</Text>
-            <IconButton
-              icon={<img src={deleteIcon} alt="Trash icon" height={20} width={20} />}
-              aria-label='Delete Habit'
-              variant='ghost'
-            />
-          </Flex>
-          <Grid
-            templateColumns='repeat(7, 1fr)'
-            borderRadius='6px'
-            bg='var(--tg-theme-bg-color)'
-            padding='2'
-            marginBottom="5"
-            placeItems="center"
-          >
-            {weekDates.map(({ day }) => (
-              <GridItem placeItems="center" fontSize="xs">{day}</GridItem>
-            ))}
-            {weekDates.map(({ day, date }) => {
-              const foundItem = history.some((item) => item.day === day && item.date === date && selectedWeekDays.includes(day));
-              let result;
-              if (foundItem) result = foundItem.success;
-              return <DayState isDone={result} />
-            })}
-          </Grid>
-        </Box>
+      {isLoading && <Box position="relative"><Spinner size='md' position="fixed" left="50%"/></Box>}
+      {habits && Array.isArray(habits) && habits.map((habitInfo, i) => (
+        <HabitCard habitInfo={habitInfo} />
       ))}
     </>
   );
---
+++ src/context/HabitsContext.js
@@ -0,0 +1,74 @@
+import { createContext, useState, useEffect } from "react";
+import useCloudStorage from "../hooks/useCloudStorage";
+import { v4 as uuidv4 } from 'uuid';
+
+export const HabitsContext = createContext();
+
+export function HabitsContextProvider({children}) {
+  const [habits, setHabits] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const cloudStorage = useCloudStorage();
+
+  useEffect(() => {
+    cloudStorage.getArray('habits')
+      .then((response) => {
+        setHabits(response);
+        setIsLoading(false);
+      });
+  }, []);
+  
+  const saveHabit = async (habitInfo) => {
+    const { name, icon, color, selectedWeekDays } = habitInfo;
+    const newHabit = {
+      id: uuidv4(),
+      name,
+      icon,
+      color,
+      selectedWeekDays: Object.entries(selectedWeekDays).reduce((acc, [day, isSelected]) => {
+        if (isSelected) acc.push(day);
+        return acc;
+      }, []),
+      history: [],
+    };
+    await cloudStorage.pushItem('habits', newHabit);
+    const newArray = [newHabit, ...habits];
+    setHabits(newArray);
+  }
+
+  const deleteHabit = async (habitId) => {
+    await cloudStorage.removeArrayItem('habits', ({id}) => id === habitId);
+    let newArray = [...habits];
+    newArray = newArray.filter(({id}) => id !== habitId);
+    setHabits(newArray);
+  }
+
+  const updateHabitStatus = async ({ habitId, date, success }) => {
+    const habitsArray = await cloudStorage.getArray('habits');
+    const habitIndex = habitsArray.findIndex(({id}) => id === habitId);
+    if (habitIndex) {
+      const foundHistoryItemIndex = habitsArray[habitIndex].history
+        .find((historyItem) => date === historyItem.date);
+      if (foundHistoryItemIndex) {
+        habitsArray[habitIndex].history[foundHistoryItemIndex].success = success;
+        await cloudStorage.setArray(habitsArray);
+        setHabits(habitsArray);
+      }
+    } else {
+      throw new Error('Habit not found');
+    }
+  }
+  const contextValues = {
+    habits,
+    setHabits,
+    isLoading,
+    setIsLoading,
+    updateHabitStatus,
+    saveHabit,
+    deleteHabit,
+  }
+  return (
+    <HabitsContext.Provider value={contextValues}>
+      {children}
+    </HabitsContext.Provider>
+  )
+}
