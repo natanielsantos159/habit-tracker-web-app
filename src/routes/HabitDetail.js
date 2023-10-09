b29387c32afdadcfff0728f7c18175d150955aca
Nataniel Santos <nathan.santos159@hotmail.com>
2023-10-09 02:05:13 -0300

    feat: Implements BackButton support on Habit Detail page

--- src/routes/HabitDetail.js
+++ src/routes/HabitDetail.js
@@ -1,7 +1,24 @@
 import { Box, Center } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Calendar from '../components/Calendar';
+import { useTelegramWebApp } from '../context/TelegramWebAppContext';
 
-function HabitDetail(props) {
+function HabitDetail() {
+  const { webApp } = useTelegramWebApp();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!webApp.BackButton.isVisible) {
+      webApp.BackButton.show();
+      webApp.BackButton.onClick(() => navigate('/'));
+    }
+
+    return () => {
+      webApp.BackButton.hide();
+    }
+  }, []);
+
   return (
     <Center>
       <Box>
