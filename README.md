1a3c8449ca83404cef5f24fcc9f0bf6193fcca36
Nataniel Santos <nathan.santos159@hotmail.com>
2023-10-10 16:41:18 -0300

    doc: Creates documentation

---
+++ README.md
@@ -0,0 +1,6 @@
+## Habit Tracker bot
+
+[@HabitTrackerRobot](https://t.me/HabitTrackerRobot) is a simple Mini App designed to help you develop and maintain positive habits in your daily life.
+
+
+[Documentation](https://github.com/natanielsantos/habit-tracker-web-app/tree/main/docs)
--- back-end/index.js
+++ back-end/index.js
@@ -1,20 +1,35 @@
-import TelegramBot from "node-telegram-bot-api";
 import { config } from "dotenv";
+import axios from "axios";
+import express from 'express'
+const app = express();
+import bodyParser from 'body-parser';
+const port = 5000;
+const apiUrl = 'https://api.telegram.org/bot';
 
 config();
 const TOKEN = process.env.BOT_TOKEN;
+const ENV = process.env.ENV;
 
-const bot = new TelegramBot(TOKEN, { polling: true });
-console.log("Bot is running...")
+console.log("Bot is running...");
 
-bot.onText(/\/start/, async (msg) => {
-    await bot.sendMessage(
-        msg.chat.id,
-        "Welcome to Habit Tracker, an simple bot designed to help you develop and maintain positive habits in your daily life.",
-        {
-            "reply_markup": {
-                "inline_keyboard": [[{ text: "Launch App 🚀", web_app: { url: process.env.WEB_APP_URL } }]]
-            }
-        }
-    );
-})
+app.use(bodyParser.json());
+const testString = ENV === 'test' ? '/test' : '';
+
+app.post('/', (req, res) => {
+    const chatId = req.body.message.chat.id;
+    const message = req.body.message.text;
+    if (message.match(/\/start/gi)) {
+        await axios.post(`${apiUrl}${TOKEN}${testString}/sendMessage`, {
+            chat_id: msg.chat.id,
+            text: "Welcome to Habit Tracker, a simple bot designed to help you develop and maintain positive habits in your daily life.",
+            reply_markup: {
+                inline_keyboard: [[{ text: "Launch App 🚀", url: process.env.WEB_APP_URL }]],
+            },
+        });
+    }
+});
+
+// Listening
+app.listen(port, () => {
+     console.log(`Listening on port ${port}`);
+});
